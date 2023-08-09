bd4132419a538abd06e481d456627d276f294aed
钱芮名 <ruiming_qian@kingdee.com>
2023-08-09 17:36:41 +0800

    优化

--- src/main/java/kd/cd/net/utils/JsonUtils.java
+++ src/main/java/kd/cd/net/utils/JsonUtils.java
@@ -1,19 +1,6 @@
 package kd.cd.net.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import lombok.SneakyThrows;
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
 
 /**
  * JSON转换工具
@@ -22,8 +9,6 @@ import java.util.List;
  * @version 1.0
  */
 public final class JsonUtils {
-    private static final SAXReader SAFE_SAX_READER = safeSaxReader();
-
     private JsonUtils() {
     }
 
@@ -102,80 +87,5 @@ public final class JsonUtils {
             valid = false;
         }
         return valid;
-    }
-
-    @SneakyThrows
-    public static ObjectNode parseXMLFile(File file) {
-        Document document = SAFE_SAX_READER.read(file);
-        return parseXML(document);
-    }
-
-    @SneakyThrows
-    public static ObjectNode parseXMLInputStream(InputStream in) {
-        Document document = SAFE_SAX_READER.read(in);
-        return parseXML(document);
-    }
-
-    @SneakyThrows
-    public static ObjectNode parseXML(String xml) {
-        Document document = DocumentHelper.parseText(xml);
-        return parseXML(document);
-    }
-
-    @NotNull
-    public static ObjectNode parseXML(Document document) {
-        ObjectNode json = JacksonUtils.getObjectMapper().createObjectNode();
-        Element root = document.getRootElement();
-        iterNodes(root, json);
-        return json;
-    }
-
-    private static void iterNodes(Element node, ObjectNode json) {
-        String nodeName = node.getName();
-        if (JacksonUtils.getKeySet(json).contains(nodeName)) {
-            JsonNode o = json.get(nodeName);
-            ArrayNode array;
-            if (o instanceof ArrayNode) {
-                array = (ArrayNode) o;
-            } else {
-                array = JacksonUtils.getObjectMapper().createArrayNode();
-                array.add(o);
-            }
-            List<Element> listElement = node.elements();
-            if (listElement.isEmpty()) {
-                String nodeValue = node.getTextTrim();
-                array.add(nodeValue);
-                json.set(nodeName, array);
-                return;
-            }
-            ObjectNode newJson = JacksonUtils.getObjectMapper().createObjectNode();
-            for (Element e : listElement) {
-                iterNodes(e, newJson);
-            }
-            array.add(newJson);
-            json.set(nodeName, array);
-            return;
-        }
-        List<Element> listElement = node.elements();
-        if (listElement.isEmpty()) {
-            String nodeValue = node.getTextTrim();
-            json.put(nodeName, nodeValue);
-            return;
-        }
-        ObjectNode object = JacksonUtils.getObjectMapper().createObjectNode();
-        for (Element e : listElement) {
-            iterNodes(e, object);
-        }
-        json.set(nodeName, object);
-    }
-
-    @SneakyThrows
-    private static SAXReader safeSaxReader() {
-        SAXReader saxReader = new SAXReader();
-        saxReader.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        saxReader.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-        saxReader.setFeature("http://xml.org/sax/features/external-general-entities", false);
-        saxReader.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-        return saxReader;
     }
 }
